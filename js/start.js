65dd8118d4e12175d670e4f32536a84e76d86512
JeongDall <ekdms12000@naver.com>
2021-05-25 14:11:36 +0900

    first commit

---
+++ js/start.js
@@ -0,0 +1,112 @@
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+const result = document.querySelector("#result");
+const endPoint = 12; 
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+//메인화면에서 질문화면으로 전환
+function begin(){
+	main.style.WebkitAnimation = "fadeOut 1s";
+	main.style.animation = "fadeOut 1s";
+	setTimeout(() => {
+		qna.style.WebkitAnimation = "fadeIn 1s";
+	    qna.style.animation = "fadeIn 1s";
+		setTimeout(() => {
+			main.style.display = "none";
+			qna.style.display = "block";
+		}, 450)
+        let qIdx = 0;   
+		goNext(qIdx);
+	}, 450);
+}
+
+function goNext(qIdx){
+	if(qIdx === endPoint){
+		goResult();
+		return;
+	}
+      //qbox에 질문이 나오게 함.
+	var q = document.querySelector('.qBox');
+	q.innerHTML = qnaList[qIdx].q;
+	for(let i in qnaList[qIdx].a){
+		addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
+	}
+	//진행상황을 알려줄 상태바 만들어줌.
+	var status = document.querySelector('.statusBar');
+	status.style.width = (100/endPoint) * (qIdx+1) + '%';
+	//페이지 번호를 표시함.
+	var pN = document.querySelector('.pageNumber');
+	pN.innerHTML = qnaList[qIdx].n;
+
+}
+
+//answerbox에 버튼형 선택지를 만듦.
+function addAnswer(answerText, qIdx, idx){
+	var a = document.querySelector('.answerBox');
+	var answer = document.createElement('button');
+	answer.classList.add('answerList');
+	answer.classList.add('my-4');
+	answer.classList.add('py-3');
+	answer.classList.add('mx-auto');
+	answer.classList.add('fadeIn');
+	
+	a.appendChild(answer);
+	answer.innerHTML = answerText;
+
+ //선택지를 누르면 사라지고 다음 질문으로 넘어가게 함.
+	answer.addEventListener("click", function(){
+		var children = document.querySelectorAll('.answerList');
+		for(let i = 0; i < children.length; i++){
+			children[i].disabled = true;
+			children[i].style.WebkitAnimation = "fadeOut 1s";
+	        children[i].style.animation = "fadeOut 1s";
+		}
+		setTimeout(() => {
+			var target = qnaList[qIdx].a[idx].type;
+			for(let i = 0; i < target.length; i++){
+				select[target[i]] += 1;
+			}
+
+			for(let i = 0; i < children.length; i++){
+			  children[i].style.display = 'none';
+		  }
+		  goNext(++qIdx);
+		}, 450);
+	}, false);
+}
+
+function calResult(){
+	console.log(select);
+	var result = select.indexOf(Math.max(...select));
+	return result;
+}
+
+function setResult(){
+	let point = calResult();
+	const resultName = document.querySelector('.resultname');
+	resultName.innerHTML = infoList[point].name;
+
+	var resultImg = document.createElement('img');
+	const imgDiv = document.querySelector('#resultImg');
+	var imgURL = 'img/image-' + point + '.png';
+	resultImg.src = imgURL;
+	resultImg.alt = point;
+	resultImg.classList.add('img-fluid');
+	imgDiv.appendChild(resultImg);
+
+	const resultDesc = document.querySelector('.resultDesc');
+	resultDesc.innerHTML = infoList[point].desc;
+}
+
+function goResult(){
+	qna.style.WebkitAnimation = "fadeOut 1s";
+	qna.style.animation = "fadeOut 1s";
+	setTimeout(() => {
+		result.style.WebkitAnimation = "fadeIn 1s";
+	    result.style.animation = "fadeIn 1s";
+		setTimeout(() => {
+			qna.style.display = "none";
+			result.style.display = "block";
+		}, 450)})
+		setResult();
+}
